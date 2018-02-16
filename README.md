db73d1c8cbbb05b2151e92cbf0dfeef6c0735d8a
RWOverdijk <r.w.overdijk@gmail.com>
2018-02-16 21:01:06 +0100

    chore(project): initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Wesley pun generator
+
+Yes, I put way too much effort in this joke.
---
+++ index.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+console.log('This app will soon depart from Terminal 1. Get it? Terminal? Ha!');
